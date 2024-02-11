c55ae20c2b30788051a7aea484af7cc523972763
Ian Ruffolo <ian.ruffolo@gmail.com>
2024-02-11 09:02:31 -0500

    added test and metric code

--- bert.py
+++ bert.py
@@ -1,7 +1,11 @@
-# import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+import pandas as pd
+
 from sklearn.model_selection import train_test_split
 from sklearn.utils.class_weight import compute_class_weight
-import numpy as np
+from sklearn.metrics import classification_report, accuracy_score
 
 import torch
 import torch.optim as optim
@@ -18,7 +22,7 @@ from load_data.database import DatabaseLoader
 
 class BERT_Arch(nn.Module):
 
-    def __init__(self, n_classes, freeze_bert=True):
+    def __init__(self, n_classes, freeze_bert=True, fine_tune=True):
 
         super(BERT_Arch, self).__init__()
         # Instantiating BERT model object
@@ -29,6 +33,11 @@ class BERT_Arch(nn.Module):
         if freeze_bert:
             for p in self.bert.parameters():
                 p.requires_grad = False
+
+            # If fine tuning bert, turn on only last layer.
+            if fine_tune:
+                for param in self.bert.encoder.layer[-1].parameters():
+                    param.requires_grad = True
 
         self.bert_drop_1 = nn.Dropout(0.3)
         self.fc = nn.Linear(self.bert.config.hidden_size,
@@ -156,9 +165,9 @@ if __name__ == "__main__":
     test_tok = tokenizer(list(test_df['clean_notes'].values),
                          return_tensors='pt', padding=True)
 
-    train_dataset = NotesDataset(train_tok, train_df['binary_outcome'])
-    val_dataset = NotesDataset(val_tok, val_df['binary_outcome'])
-    test_dataset = NotesDataset(test_tok, test_df['binary_outcome'])
+    train_dataset = NotesDataset(train_tok, train_df['OUTCOME'])
+    val_dataset = NotesDataset(val_tok, val_df['OUTCOME'])
+    test_dataset = NotesDataset(test_tok, test_df['OUTCOME'])
 
     classifier = BertClassifier()
     classifier.to(device)
@@ -218,8 +227,8 @@ if __name__ == "__main__":
     # compute the class weights
     class_wts = compute_class_weight(
         class_weight='balanced',
-        classes=np.unique(data['binary_outcome'].values.tolist()),
-        y=data['binary_outcome'])
+        classes=np.unique(data['OUTCOME'].values.tolist()),
+        y=data['OUTCOME'])
 
     # convert class weights to tensor
     weights = torch.tensor(class_wts, dtype=torch.float)
@@ -232,20 +241,19 @@ if __name__ == "__main__":
     train_seq = torch.tensor(train_encoding['input_ids'])
     train_mask = torch.tensor(train_encoding['attention_mask'])
     train_token_ids = torch.tensor(train_encoding['token_type_ids'])
-    train_y = torch.tensor(train_df['binary_outcome'].tolist())
+    train_y = torch.tensor(train_df['OUTCOME'].tolist())
 
     val_seq = torch.tensor(val_encoding['input_ids'])
     val_mask = torch.tensor(val_encoding['attention_mask'])
     val_token_ids = torch.tensor(val_encoding['token_type_ids'])
-    val_y = torch.tensor(val_df['binary_outcome'].tolist())
+    val_y = torch.tensor(val_df['OUTCOME'].tolist())
 
     test_seq = torch.tensor(test_encoding['input_ids'])
     test_mask = torch.tensor(test_encoding['attention_mask'])
     test_token_ids = torch.tensor(test_encoding['token_type_ids'])
-    test_y = torch.tensor(test_df['binary_outcome'].tolist())
+    test_y = torch.tensor(test_df['OUTCOME'].tolist())
 
     BATCH_SIZE = 16
-    NUM_WORKERS = 2
     learning_rate = 1e-3
     steps_per_epoch = 20
 
@@ -254,34 +262,34 @@ if __name__ == "__main__":
     traindata = DataLoader(
         train_data,
         batch_size=BATCH_SIZE,
-        num_workers=NUM_WORKERS,
         sampler=RandomSampler(train_data),
-        pin_memory=True
+        pin_memory=True,
+        drop_last=True,
     )
 
     val_data = TensorDataset(val_seq, val_mask, val_token_ids, val_y)
     valdata = DataLoader(
         val_data,
         batch_size=BATCH_SIZE,
-        num_workers=NUM_WORKERS,
         sampler=SequentialSampler(val_data),
-        pin_memory=True
+        pin_memory=True,
+        drop_last=True
     )
 
     test_data = TensorDataset(test_seq, test_mask, test_token_ids, test_y)
     testdata = DataLoader(
         test_data,
         batch_size=BATCH_SIZE,
-        num_workers=NUM_WORKERS,
         sampler=SequentialSampler(test_data),
-        pin_memory=True
+        pin_memory=True,
+        drop_last=True
     )
 
     print('Number of data in the train set', len(traindata))
     print('Number of data in the validation set', len(valdata))
     print('Number of data in the test set', len(testdata))
 
-    class_names = np.unique(data['binary_outcome'])
+    class_names = np.unique(data['OUTCOME'])
     print('Downloading the BERT custom model...')
     model = BERT_Arch(len(class_names))
     model.to(device)  # Model to GPU.
@@ -417,7 +425,7 @@ if __name__ == "__main__":
     valid_losses = []
 
     # for each epoch perform training and evaluation
-    epochs = 1
+    epochs = 10
     for epoch in range(epochs):
 
         print('\n Epoch {:} / {:}'.format(epoch + 1, epochs))
@@ -443,25 +451,119 @@ if __name__ == "__main__":
         print(f'\nTraining Loss: {train_loss:.3f}')
         print(f'Validation Loss: {valid_loss:.3f}')
 
-    # Fine Tuning
-    # losses = []
-    #
-    # for epoch in range(10):
-    #     all_loss = 0
-    #     for idx, batch in enumerate(train_dataset):
-    #         batch_loss = 0
-    #         classifier.zero_grad()
-    #         input_ids = batch['input_ids'].to(device)
-    #         attention_mask = batch['attention_mask'].to(device)
-    #         labels = batch['labels'].to(device)
-    #
-    #         out = classifier(input_ids, attention_mask)
-    #
-    #         batch_loss = loss_function(out, labels)
-    #         batch_loss.backward()
-    #         optimizer.step()
-    #
-    #         all_loss += batch_loss.item()
-    #     print("epoch", epoch, "\t", "loss", all_loss)
+    print('\nTest Set...')
 
-    # model.eval()
+    test_preds = []
+
+    print('Total batches:', len(testdata))
+
+    for fold_index in range(0, 3):
+
+        print('\nFold Model', fold_index)
+
+        # Load the fold model
+        path_model = 'bert_weights.pt'
+        model.load_state_dict(torch.load(path_model))
+
+        # Send the model to the GPU
+        model.to(device)
+
+        stacked_val_labels = []
+
+        # Put the model in evaluation mode.
+        model.eval()
+
+        # Turn off the gradient calculations.
+        # This tells the model not to compute or store gradients.
+        # This step saves memory and speeds up validation.
+        torch.set_grad_enabled(False)
+
+        # Reset the total loss for this epoch.
+        total_val_loss = 0
+
+        for j, test_batch in enumerate(testdata):
+
+            inference_status = 'Batch ' + str(j + 1)
+
+            print(inference_status, end='\r')
+
+            b_input_ids = test_batch[0].to(device)
+            b_input_mask = test_batch[1].to(device)
+            b_token_type_ids = test_batch[2].to(device)
+            b_test_y = test_batch[3].to(device)
+
+            outputs = model(b_input_ids,
+                            attention_mask=b_input_mask,
+                            token_type_ids=b_token_type_ids)
+
+            # Get the preds
+            preds = outputs[0]
+
+            # Move preds to the CPU
+            val_preds = preds.detach().cpu().numpy()
+
+            # true_labels.append(b_test_y.to('cpu').numpy().flatten())
+
+            # Stack the predictions.
+            if j == 0:  # first batch
+                stacked_val_preds = val_preds
+
+            else:
+                stacked_val_preds = np.vstack((stacked_val_preds, val_preds))
+
+        test_preds.append(stacked_val_preds)
+
+    print('\nPrediction complete.')
+    print(len(test_preds))
+    print(test_preds[:5])
+
+    # Sum the predictions of all fold models
+    for i, item in enumerate(test_preds):
+        if i == 0:
+            preds = item
+        else:
+            # Sum the matrices
+            preds = item + preds
+
+    # Average the predictions
+    avg_preds = preds/(len(test_preds))
+
+    # print(preds)
+    # print()
+    # print(avg_preds)
+
+    # Take the argmax.
+    # This returns the column index of the max value in each row.
+    test_predictions = np.argmax(avg_preds, axis=1)
+
+    # Take a look of the output
+    print(type(test_predictions))
+    print(len(test_predictions))
+    print()
+    print(test_predictions)
+
+    true_y = []
+    for j, test_batch in enumerate(testdata):
+        true_y.append(int(test_batch[3][0].numpy().flatten()))
+    print(true_y)
+
+    # Accuracy and classification report
+    target_names = ['true_y', 'predicted_y']
+
+    data = {'true_y': true_y,
+            'predicted_y': test_predictions}
+
+    df_pred_BERT = pd.DataFrame(data, columns=['true_y', 'predicted_y'])
+
+    confusion_matrix = pd.crosstab(df_pred_BERT['true_y'],
+                                   df_pred_BERT['predicted_y'], rownames=[
+                                   'True'], colnames=['Predicted'])
+
+    print('Accuracy of BERT model', accuracy_score(true_y, test_predictions))
+    print(classification_report(true_y,
+                                test_predictions,
+                                target_names=target_names,
+                                labels=class_names))
+
+    sns.heatmap(confusion_matrix, annot=True)
+    plt.show()
